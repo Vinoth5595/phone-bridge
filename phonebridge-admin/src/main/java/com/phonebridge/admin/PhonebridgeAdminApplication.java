2d9f8641efe52b68fd0925dcf00e1ec6a4cc4320
Vinoth Manoharan <vinothm.manoharan@gamil.com>
2020-03-15 13:58:45 +0530

    changed localhost to phonebridge-admin (Eureka server registered name)

--- phonebridge-admin/src/main/java/com/phonebridge/admin/PhonebridgeAdminApplication.java
+++ phonebridge-admin/src/main/java/com/phonebridge/admin/PhonebridgeAdminApplication.java
@@ -2,11 +2,13 @@ package com.phonebridge.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @EnableAdminServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PhonebridgeAdminApplication {
 
 	public static void main(String[] args) {
