eaf55a5df53c38779332915a336d8a14bda9a34f
Vinoth Manoharan <vinothm.manoharan@gamil.com>
2020-04-22 03:37:31 +0530

    Added elastic stack for logging and added the services in docker compose file. Docker compose is not completely done.

--- phonebridge-authserver/src/test/java/com/phonebridge/authserver/AuthserverApplicationTests.java
+++ phonebridge-authserver/src/test/java/com/phonebridge/authserver/AuthserverApplicationTests.java
@@ -1,13 +1,9 @@
 package com.phonebridge.authserver;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AuthserverApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
